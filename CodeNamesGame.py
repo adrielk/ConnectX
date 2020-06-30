86bcc74ad11f5430cf1dd6be63ef248bf3e8e9ec
Adriel <59209071+adrielk@users.noreply.github.com>
2020-06-30 00:34:12 -0400

    constructKeyCard function

--- CodeNamesGame.py
+++ CodeNamesGame.py
@@ -6,6 +6,12 @@ Code Names - Text Edition
 
 Graphical edition will come soon once I learn how to use pygame....
 
+To do:
+    - Document code
+    - Put together building blocks to make the game!
+    - Interface keycard with constructRandomWordGrid. (make back of word cards equal to corresponding keycard value)
+    
+
 @author: Adriel Kim
 """
 
@@ -52,7 +58,9 @@ def generateAllIndices(grid):
         for c in range(0, grid.shape[1]):
             indices.append((r,c))
     return indices
-
+"""
+Returns a list of adjacent values relative to a point on a grid
+"""
 def getAdjacentValues(grid, index):
     adjacents = []
     gridDimR = grid.shape[0]
@@ -61,21 +69,21 @@ def getAdjacentValues(grid, index):
     col = index[1]
     
     if(row+1 < gridDimR):
-        adjacents.append(grid[row+1, col])
+        adjacents.append((row+1, col))
         if((col+1)<gridDimC):
-            adjacents.append(grid[row+1, col])
+            adjacents.append((row+1, col))
     if(row-1 >= 0):
-        adjacents.append(grid[row-1,col])
+        adjacents.append((row-1,col))
         if(col+1<gridDimC):
-            adjacents.append(grid[row-1,col+1])
+            adjacents.append((row-1,col+1))
     if(col+1 < gridDimC):
-        adjacents.append(grid[row, col+1])
+        adjacents.append((row, col+1))
     if(col-1 >= 0):
-        adjacents.append(grid[row, col-1])
+        adjacents.append((row, col-1))
         if(row+1<gridDimR):
-            adjacents.append(grid[row+1, col-1])
+            adjacents.append((row+1, col-1))
         if(row-1 >=0):
-            adjacents.append(grid[row-1,col-1])
+            adjacents.append((row-1,col-1))
             
     return adjacents
     
@@ -88,29 +96,45 @@ Maybe a better implementaiton would be making the back of each gard be the agent
 There will be certain probabilities of clustering. a clustering of 2 will have a high chance, 3 will have a lower chance, and 4 
 will have an even lower chance. A clustering of 1 has hte lowest chance.
 
-OR probability of being red is based on number of red adjacent
+OR probability of being red is based on number of red 
+
+To do:
+    FIX. Should have exactly rspot and bspots, but it varies!
 """
 def constructKeyCard(dims, rSpots, bSpots, rProb, bProb):
     keyCard = np.full((dims[0],dims[1]), "Wrong_Guess")
     possibleIndices = generateAllIndices(keyCard)
     
     
-    for i in range(0, rSpots):
+    while(rSpots > 0):
         randIndex = randint(0, len(possibleIndices)-1)
         randRow = possibleIndices[randIndex][0]
         randCol = possibleIndices[randIndex][1]
+        adjacents = getAdjacentValues(keyCard, (randRow, randCol))
         
-       #adjacents = getAdjacentValues(keyCard, randIndex)
         keyCard[randRow, randCol] = "Red_Agent"
+        rSpots -=1
+        for a in adjacents:
+            randNum = randint(0,100)
+            if rSpots > 0 and keyCard[a[0],a[1]] == "Wrong_Guess" and randNum <= rProb*100:
+                keyCard[a[0],a[1]] = "Red_Agent"
+                rSpots -= 1
         
         possibleIndices.remove(possibleIndices[randIndex])
         
-    for i in range(0, bSpots):
+    while(bSpots > 0):
         randIndex = randint(0, len(possibleIndices)-1)
         randRow = possibleIndices[randIndex][0]
         randCol = possibleIndices[randIndex][1]
+        adjacents = getAdjacentValues(keyCard, (randRow, randCol))
         
         keyCard[randRow, randCol] = "Blue_Agent"
+        bSpots -=1
+        for a in adjacents:
+            randNum = randint(0,100)
+            if bSpots > 0 and a in possibleIndices and keyCard[a[0],a[1]] == "Wrong_Guess" and randNum <= bProb*100:
+                keyCard[a[0],a[1]] = "Blue_Agent"
+                bSpots -= 1
         
         possibleIndices.remove(possibleIndices[randIndex])
     
@@ -129,3 +153,6 @@ wordGrid.printboard()
 
 keyCard = constructKeyCard((5,5),8, 9, 0.7,0.7)
 print(keyCard)
+unique, counts = np.unique(keyCard, return_counts = True)
+print(counts)
+print(unique)
